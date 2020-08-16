dcb1d4c4e429b978942bc3126f0a14f18fde87fb
Alex Mills <agjmills@users.noreply.github.com>
2020-08-16 13:18:20 +0100

    added commands to arm and disarm the motion detection

--- securitybot.py
+++ securitybot.py
@@ -8,9 +8,16 @@ import os
 import glob
 from telepot.loop import MessageLoop
 
+def webcontrol(chat_id, type, cmd):
+    req = 'http://localhost:8080/0/'+type+'/'+cmd
+    res = requests.get(req)
+    bot.sendMessage(chat_id, res.text)
+
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
+    #should work thanks to Winston
+    print(msg['from']['id'])
     if msg['from']['id'] != YOUR_TELEGRAM_ID:
         bot.sendMessage(chat_id, "Sorry this is a personal bot. Access Denied!")
         exit(1)
@@ -19,10 +26,18 @@ def handle(msg):
 
     if command == '/time':
         bot.sendMessage(chat_id, 'The current time is: '+str(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')))
+    elif command == '/disarm':
+        bot.sendMessage(chat_id, 'Disarming motion detection')
+        os.system('sudo supervisorctl stop motion')
+        bot.sendMessage(chat_id, 'Motion detection disarmed')
+    elif command == '/arm':
+        bot.sendMessage(chat_id, 'Arming motion detection')
+        os.system('sudo supervisorctl start motion')
+        bot.sendMessage(chat_id, 'Motion detection armed')
     else:
         bot.sendMessage(chat_id, "sorry, I don't know the command "+command)
 # adapt the following to the bot_id:bot_token
-bot = telepot.Bot(YOUR_TELEGRAM_TOKEN')
+bot = telepot.Bot(YOUR_TELEGRAM_KEY)
 MessageLoop(bot, handle).run_as_thread()
 print('I am listening ...')
 
